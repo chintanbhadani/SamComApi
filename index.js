a427c68f151f36bea09cedb490bd6802a09b71e0
Chintan <chintanbhadani9898@gmail.com>
2021-10-26 12:17:42 +0530

    initial

--- index.js
+++ index.js
@@ -8,24 +8,6 @@ const PORT = 8000
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));  
 
-// mongoose.connect('mongodb://localhost:27017/BCCI', 
-// { useNewUrlParser: true }, 
-// (err) => {
-//     if (!err) { console.log('MongoDB Connection Succeeded.') }
-//     else { console.log('Error in DB connection : ' + err) }
-// });
-
-
-// const { MongoClient } = require('mongodb');
-// const uri = "mongodb+srv://chintan:chintan@123@cluster0.ovz1t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//     console.log('MongoDB Connection Succeeded.')
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
 
 // mongodb+srv://chintanB:1234@cluster0.ovz1t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/RTO
 mongoose.connect('mongodb+srv://chintanB:1234@cluster0.ovz1t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
